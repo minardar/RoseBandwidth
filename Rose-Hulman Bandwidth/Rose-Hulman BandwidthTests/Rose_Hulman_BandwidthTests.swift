7ebbc7b4bdbca0463f49871619f9651fba31a081
jungckjp <jungckjp@rose-hulman.edu>
2015-01-28 15:17:29 -0500

    Sprint 0 Work almost complete
    Ballin’

---
+++ Rose-Hulman Bandwidth/Rose-Hulman BandwidthTests/Rose_Hulman_BandwidthTests.swift
@@ -0,0 +1,36 @@
+//
+//  Rose_Hulman_BandwidthTests.swift
+//  Rose-Hulman BandwidthTests
+//
+//  Created by Jonathan Jungck on 1/28/15.
+//  Copyright (c) 2015 Jonathan Jungck and Anthony Minardo. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Rose_Hulman_BandwidthTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
